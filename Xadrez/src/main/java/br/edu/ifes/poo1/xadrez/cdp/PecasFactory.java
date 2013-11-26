54edd5a893fd0dca82595686dcd89d8316378eab
Phillipe Lopes <>
2013-11-26 11:16:19 -0200

    Comentários nos métodos da classe PecasFactory.

--- Xadrez/src/main/java/br/edu/ifes/poo1/xadrez/cdp/PecasFactory.java
+++ Xadrez/src/main/java/br/edu/ifes/poo1/xadrez/cdp/PecasFactory.java
@@ -13,11 +13,17 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Fábrica de Peças.
  * @author leds
  */
 public class PecasFactory {
 
+    /**
+     *
+     * @param quantidade Quantidade de pecas a serem criadas.
+     * @param nomePeca Nome da classe desejada. Ex.: "Cavalo", "Peao".
+     * @return Um mapa com um vetor da peça desejada relacionado a uma cor.
+     */
     public Map<Cor, Peca[]> getPecas(int quantidade, String nomePeca) {
         Map<Cor, Peca[]> pecas;
         pecas = this.criarMapPecas(quantidade);
@@ -26,26 +32,6 @@ public class PecasFactory {
         } catch (Exception ex) {
             Logger.getLogger(PecasFactory.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return pecas;
-    }
-
-    /**
-     *
-     * @param quantidade A quantidade total de peças que se deseja criar.
-     * @return Um Map que a cada cor associada, retorna um vetor vazio do tipo
-     * Peça.
-     */
-    private Map<Cor, Peca[]> criarMapPecas(int quantidade) {
-        Peca[] pecasBrancas;
-        Peca[] pecasPretas;
-        Map<Cor, Peca[]> pecas;
-        pecasBrancas = new Peca[quantidade];
-        pecasPretas = new Peca[quantidade];
-
-        pecas = new HashMap<>();
-        pecas.put(Cor.BRANCO, pecasBrancas);
-        pecas.put(Cor.PRETO, pecasPretas);
-
         return pecas;
     }
 
@@ -72,5 +58,25 @@ public class PecasFactory {
             pecasPretas[nPeca] = new Torre();
             pecasPretas[nPeca].setCor(Cor.PRETO);
         }
+    }
+
+    /**
+     *
+     * @param quantidade A quantidade total de peças que se deseja criar.
+     * @return Um Map que a cada cor associada, retorna um vetor vazio do tipo
+     * Peça.
+     */
+    private Map<Cor, Peca[]> criarMapPecas(int quantidade) {
+        Peca[] pecasBrancas;
+        Peca[] pecasPretas;
+        Map<Cor, Peca[]> pecas;
+        pecasBrancas = new Peca[quantidade];
+        pecasPretas = new Peca[quantidade];
+
+        pecas = new HashMap<>();
+        pecas.put(Cor.BRANCO, pecasBrancas);
+        pecas.put(Cor.PRETO, pecasPretas);
+
+        return pecas;
     }
 }
