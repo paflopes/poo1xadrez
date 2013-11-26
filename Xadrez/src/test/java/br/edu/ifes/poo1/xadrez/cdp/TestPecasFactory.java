27f94bac6e1101d20dd6d65c70cb4199a2e0c95e
LEDS <leds.ifes@gmail.com>
2013-11-26 12:59:45 -0200

    Criação de Testes.

---
+++ Xadrez/src/test/java/br/edu/ifes/poo1/xadrez/cdp/TestPecasFactory.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifes.poo1.xadrez.cdp;
+
+import br.edu.ifes.poo1.xadrez.cdp.pecas.Peca;
+import java.util.Map;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author leds
+ */
+public class TestPecasFactory {
+
+    PecasFactory pecasFactory;
+    Map<Cor, Peca[]> pecas;
+
+    @Before
+    public void before() {
+        this.pecasFactory = new PecasFactory();
+        this.pecas = this.pecasFactory.getPecas(16, "Peao");
+    }
+
+    @Test
+    public void testGetPecas() {
+        Assert.assertNotNull(this.pecas);
+
+        for (int nPeca = 0; nPeca < 9; nPeca++) {
+            Assert.assertEquals(Cor.BRANCO, this.pecas.get(Cor.BRANCO)[nPeca].getCor());
+        }
+        for (int nPeca = 0; nPeca < 9; nPeca++) {
+            Assert.assertEquals(Cor.PRETO, this.pecas.get(Cor.PRETO)[nPeca].getCor());
+        }
+
+    }
+
+}
